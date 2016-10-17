8afc8c7165113e6b2d4a14fb2370a3dc1569ddcd
mariocs96 <mario_96_cs@hotmail.com>
2016-10-17 12:17:11 +0200

    change

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-// Algunos requires necesarios
-var path = require('path');
 
-// Creamos el servidor web usando Express dentro del entorno de Node.js
+
+var path = require('path');
 var express = require('express');
 var app = express();
 
-//Indicamos al servidor que cualquier fichero que le pidan
-//lo podrá encontrar en la carpeta clientFiles
-app.use(express.static('clientFiles'));
 
 
-// En caso de que la ruta web sea la raiz
+
+
+app.use(express.static("clientFiles/js"));
+
 app.get('/', function(req, res) {
-    // Devolvemos el fichero index de la carpeta de ficheros del cliente
-    res.sendFile(path.join(__dirname + '/clientFiles/index.html'));
+  res.sendFile(path.join(__dirname + '/clientFiles/index.html'));
 });
-
-// Escuchamos en el puerto 8000
-app.listen(8000);
+app.listen(3000, function(){
+  console.log('Server Express Ready!');
+});
